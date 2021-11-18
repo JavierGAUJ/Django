4242a8afa8721a6ae54c421ac7e454ea13d601ec
JavierGAUJ <rockman491Javier@gmail.com>
2021-11-17 22:46:43 -0600

    alarmas9

--- hola/alarmas/views.py
+++ hola/alarmas/views.py
@@ -1,11 +1,9 @@
-from django.urls import reverse
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
-
-# Create your views here.
 
 def index(request):
     if "alarmas " not in request.session:
