99fbf479a0d0e406bdb671744e552f2b169a292e
JavierGAUJ <rockman491Javier@gmail.com>
2021-11-30 21:01:42 -0600

    vuelo 1

--- hola/aereolina/views.py
+++ hola/aereolina/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from .models import flight
+from .models import Flight
 
 # Create your views here.
 
 def index (request):
    return render(request, 'aereolina/index.html', {
-      "flights": flight.objects.all()
+      "flights": Flight.objects.all()
    })
 def flight (request, flight_id):
-   flight = flight.objects.get (id=flight_id)
+   flight = Flight.objects.get (id=flight_id)
    return render(request, 'aereolina/flight.html', {
       'flight':flight
    })
